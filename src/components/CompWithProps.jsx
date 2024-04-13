d5f926a2859ef130a3c0471c214fa50e5fe0ea16
Rocio Oquendo <oquendoramirezrocio@gmail.com>
2024-04-13 19:32:41 -0400

    update

--- src/components/CompWithProps.jsx
+++ src/components/CompWithProps.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 export const CompWithProps = () => {
     const [texto, setTexto] = useState("");
 
-    const updateWord = (word) => {
-        setTexto(word.target.value);
+    const updateText = (text) => {
+        setTexto(text.target.value);
     };
 
     return (
         <div>
-            <input type="text" value={texto} onChange={updateWord} />
+            <input type="text" value={texto} onChange={updateText} />
             <div>
                 <p><b>User: </b>{texto}</p>
             </div>
