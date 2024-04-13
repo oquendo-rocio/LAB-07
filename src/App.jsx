ce3013d202296f881f33c5a29faf31db6f73191d
Rocio Oquendo <oquendoramirezrocio@gmail.com>
2024-04-13 16:33:23 -0400

    add lab-07 to the new repository

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { WapperList } from './components/WrapperList';
+import { List } from './components/List';
+
+function App() {
+
+  return (
+    <>
+      <WapperList>
+        <List/>
+      </WapperList>
+    </>
+  )
+}
+
+export default App
